7df91bb1e33923139fb4aa14f3964ed8be22afd1
Nicolas <nikoalegre@hotmail.com>
2020-11-06 16:00:48 -0300

    add the function for get all the pokemon and the data for one pokemon

--- src/App.js
+++ src/App.js
@@ -2,30 +2,58 @@ import {useState, useEffect} from 'react'; //Import the hook
 import PokemonCard from './components/pokemonCard'
 import axios from 'axios' //Import axios for GET the data from the API
 
+
+
+
+
+
 function App() {
   const [pokemon, setPokemon] = useState([])
 
+  const getPokemon = (pokemonUrl) => {
+    /* return new Promise((resolve, reject) =>{
+      axios.get(pokemonUrl)
+    }) */
+    return axios.get(pokemonUrl)
+  }
+
+  const getAllPokemon = async () =>{
+    /* axios.get('https://pokeapi.co/api/v2/pokemon', {
+      params: {
+        limit: 100, //quantity of pokemon to fetch
+        offset: 0 //quantity of pokemon to skip
+      }
+    })
+    .then(res => {
+      setPokemon(res.data.results.map(p => p))
+    })
+    .catch(function (error) {
+      console.log(error);
+    }) */
+
+    let {data} = await axios.get('https://pokeapi.co/api/v2/pokemon')
+
+    
+    let pokemonData = await Promise.all(data.results.map( async p =>{
+      let pokemonRecord = await getPokemon(p.url);
+      return pokemonRecord
+    }))
+    
+    setPokemon(pokemonData.map( p => {
+      return p
+    }))
+
+  }
+
   useEffect(() => {
-    axios.get('https://pokeapi.co/api/v2/pokemon', {
-    params: {
-      limit: 100, //quantity of pokemon to fetch
-      offset: 0 //quantity of pokemon to skip
-    }
-  })
-  .then(res => {
-    setPokemon(res.data.results.map(p => p))
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
+    getAllPokemon()
   }, [])
 
 
-
-  
-
   return (
-    <PokemonCard pokemon={pokemon} />
+    <>
+    <PokemonCard pokemon={pokemon}/>
+    </>
   );
 }
 
