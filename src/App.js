13b671031f28a8720fa07dd465e5877580cea385
Nicolas <nikoalegre@hotmail.com>
2020-11-06 01:35:30 -0300

    Add the GET method and the useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-
+import {useState, useEffect} from 'react'; //Import the hook
+import PokemonCard from './components/pokemonCard'
+import axios from 'axios' //Import axios for GET the data from the API
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon', {
+    params: {
+      limit: 100, //quantity of pokemon to fetch
+      offset: 0 //quantity of pokemon to skip
+    }
+  })
+  .then(res => {
+    setPokemon(res.data.results.map(p => p))
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  }, [])
+
+
+
+  
+
   return (
-    null
+    <PokemonCard pokemon={pokemon} />
   );
 }
 
