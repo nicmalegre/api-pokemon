9898c4facc110d8826ecc9cf8dfc1f8802d765d7
Nicolas <nikoalegre@hotmail.com>
2020-11-06 16:01:57 -0300

    Edit the pokemonCard components for GET the data from the props for one pokemon

--- src/components/pokemonCard.js
+++ src/components/pokemonCard.js
@@ -6,7 +6,11 @@ const PokemonCard = (props) => {
     return ( 
         <div>
             {props.pokemon.map( p => (
-                <p key={p.name}>{p.name}</p>
+                <>
+                <p key={p.data.name}>Name: {p.data.name}</p>
+                <p key={p.data.name}>Height: {p.data.height}</p>
+                <p key={p.data.name}>Weight: {p.data.weight}</p>
+                </>
             ))}
         </div>
   );
