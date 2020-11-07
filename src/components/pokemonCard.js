17fc586196f0f352c5dcda9be7a6822f13587db4
Nicolas <nikoalegre@hotmail.com>
2020-11-07 00:35:01 -0300

    Clean the code

--- src/components/pokemonCard.js
+++ src/components/pokemonCard.js
@@ -1,22 +1,15 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 
 /* IMPORT FROM MATERIAL-UI */
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { green } from '@material-ui/core/colors';
 
 
 /* STYLES FOR THE CARD IMPORT FROM MATERIAL-UI */
 const useStyles = makeStyles({
     root: {
       maxWidth: 320,
-      minWidth:300,
+      minWidth:240,
       display: 'inline-block',
       margin: 15,
       padding: 15,
@@ -39,19 +32,19 @@ const PokemonCard = (props) => {
                 <img height='120px' src={props.pokemon.data.sprites.front_default} alt=''/>
                 <img height='120px' src={props.pokemon.data.sprites.back_default} alt=''/>
             </div>
-            <p key={props.pokemon.data.name}><b>Name:</b> {props.pokemon.data.name}</p>
-            <p key={props.pokemon.data.height}><b>Height:</b> {props.pokemon.data.height}</p>
-            <p key={props.pokemon.data.weight}><b>Weight:</b> {props.pokemon.data.weight}</p>
-            <p key={props.pokemon.data.base_experience}><b>Base Experience:</b> {props.pokemon.data.base_experience}</p>
-            <p><b>Abilities: </b>
-                <ul>
-                    {props.pokemon.data.abilities.map(a =>{ 
-                        return (
-                            <li key={a.ability.name}>{a.ability.name}</li>
-                        )
-                    })}
-                </ul>
-            </p> 
+            <p><b>Name:</b> {props.pokemon.data.name}</p>
+            <p><b>Height:</b> {props.pokemon.data.height}</p>
+            <p><b>Weight:</b> {props.pokemon.data.weight}</p>
+            <p><b>Base Experience:</b> {props.pokemon.data.base_experience}</p>
+            <p><b>Abilities: </b></p> 
+            <ul>
+                {props.pokemon.data.abilities.map((a,index) =>{ 
+                    return (
+                        <li key={index}>{a.ability.name}</li>
+                    )
+                })}
+            </ul>
+            
         </Card> 
         </>
 
