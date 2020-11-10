57f3b1c19f7c14c0f6d5a867c73a6d5bd398e2c4
Nicolas <nikoalegre@hotmail.com>
2020-11-10 17:13:59 -0300

    Improve the GET method for get the detail information for every pokemon

--- src/api/pokemonApi.js
+++ src/api/pokemonApi.js
@@ -5,3 +5,11 @@ export const getAllPokemons = async (key, count) =>{
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${count}`);
     return res.json()
 }
+
+
+//GET THE DETAIL OF ONE POKEMON FROM THE API
+//Return: pokemon
+export const getPokemon = async (key, url) =>{
+    const res = await fetch(url);
+    return res.json()
+}
